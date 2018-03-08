817704e14901590c8a23c631d146f0ec243a8f0b
FatmaKhaled <fatma10khaled10@gmail.com>
2018-03-09 00:16:48 +0200

    create otlob folder

---
+++ otlob/app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+   
+  
+
+  # Include default devise modules. Others available are:
+
+ 
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+
+   validates :name, presence: true,
+                    length: { minimum: 3 }
+
+  validates :email, uniqueness: true,
+                    length: { minimum: 3 }
+   validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+    message: "please enter valid email" }
+
+end
