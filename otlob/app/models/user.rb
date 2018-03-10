74db2739947f24ea6366699d1871942d2063521d
Eslam <eslam.92.sayed@gmail.com>
2018-03-10 21:04:23 +0200

    added friendship model,controller and view

--- otlob/app/models/user.rb
+++ otlob/app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
    
   has_and_belongs_to_many :groups
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
   # Include default devise modules. Others available are:
 
