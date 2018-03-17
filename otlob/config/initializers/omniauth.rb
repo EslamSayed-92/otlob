2924107d985fece0c3a69b1a2c1e82394ca64057
rehabrezek <rehab.r.elghannam@gmail.com>
2018-03-17 04:20:19 +0200

    some edited in log in with social media

--- otlob/config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['1788079561498907'], ENV['d236b72537f14ca1255460660']
-
-
-end
